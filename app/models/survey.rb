9578a8291a4a2089e489a33fcfba807526fdeb7e
cyanvillarin <cyanvillarin@gmail.com>
2017-10-09 15:39:46 +0800

    fixed validation bug

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -25,72 +25,72 @@ class Survey < ApplicationRecord
 	validates_numericality_of :ft, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :rp, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :rp, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :ro, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :ro, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :rs, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :rs, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :rc, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :rc, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :rm, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :rm, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :rt, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :rt, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :po, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :po, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :ps, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :ps, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :pc, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :pc, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :pm, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :pm, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :pt, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :pt, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :os, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :os, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :oc, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :oc, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :om, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :om, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :ot, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :ot, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :sc, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :sc, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :sm, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :sm, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :st, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :st, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :cm, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :cm, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :ct, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :ct, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 
 
-	validates :ft, presence: true, numericality: { only_int: true, other_than: 0 }
+	validates :mt, presence: true, numericality: { only_int: true, other_than: 0 }
 	validates_numericality_of :mt, greater_than_or_equal_to: -5, less_than_or_equal_to: 5
 end
 
