dc4b550ebdf6ea3dc3b5d32300c165179c648552
Treacy Evangelista <treacyev@gmail.com>
2017-11-17 05:33:22 -0800

    modal definitions

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+  $("#fs").click(function(){
+    $("#fsmodal").modal('show');
+  });
+
+  $("#rel").click(function(){
+    $("#relmodal").modal('show');
+  });
+
+  $("#op").click(function(){
+    $("#opmodal").modal('show');
+  });
+
+  $("#pe").click(function(){
+    $("#pemodal").modal('show');
+  });
+
+  $("#comp").click(function(){
+    $("#compmodal").modal('show');
+  });
+
+  $("#sec").click(function(){
+    $("#secmodal").modal('show');
+  });
+
+  $("#trans").click(function(){
+    $("#transmodal").modal('show');
+  });
+
+  $("#maint").click(function(){
+    $("#maintmodal").modal('show');
+  });
+
+});
